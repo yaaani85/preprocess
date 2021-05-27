545598864e80503f4ab3dca301a6fbbb6c12a6cc
yaaani85 <43518351+yaaani85@users.noreply.github.com>
2021-05-27 09:26:51 +0200

    Update test_preprocess.py

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -3,5 +3,5 @@ from ..brain_preprocessing.py import preprocess
 
 #local testing
 def test_preprocess():
-  assert preprocess(true, true) == "Preprocessed data"
+  assert preprocess(True, True) == "Preprocessed data"
   
